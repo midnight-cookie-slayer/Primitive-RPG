b24b34bca4fde58e824f71f2d24ed202a45526cc
Sergey <chloegray618@gmail.com>
2021-09-15 13:54:10 +0300

    first commit with 1 kata

---
+++ cw test.py
@@ -0,0 +1,22 @@
+"""def divisors(l):
+    h = [1, l]
+    for j in range(l-1, 1, -1):
+        if (l % j == 0):
+            h.append(j)
+    return h
+
+def lsum(k):
+    s = 0
+    for i in k:
+        s = s + i
+    return s
+
+def kv(s):
+    sqrt = 0
+    for i in range(0, 1000000):
+        sqrt = s ** (0.5)
+    if type(sqrt) == int:
+        return sqrt
+"""
+
+
